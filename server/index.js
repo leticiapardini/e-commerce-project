13e6019b19463781622787a034ba7e8a8d5fecab
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2022-06-09 13:00:03 -0300

    feat: product routes

--- server/index.js
+++ server/index.js
@@ -12,11 +12,12 @@ server.use(express.json());
 
 
 let users = [];
+let products = [];
 
 // USERS
 
 /********************
- *     Dados         *
+ *     DATA          *
  *                   *
  * id: string;       *
  * name: string      *
@@ -58,7 +59,7 @@ server.post('/users', (request, response) => {
     users.push(user);
 
     response.send({
-        conteudo: name
+        content: name
     });
 });
 
@@ -94,7 +95,97 @@ server.delete('/users/:id', (request, response) => {
     response.send({})
 });
 
-// TODO fazer processo acima para PROTUDOS(LIVROS)
+// BOOKS
+
+/**********************
+ *      DATA          *
+ *                    *
+ * id: string;        *
+ * title: string      *
+ * author: string     *
+ * publisher: string  *
+ * price: number      *
+ * year: number       *
+ **********************/
+
+//     listagem
+server.get('/products', (request, response) => {
+    response.send(products);
+})
+
+//     pesquisa
+server.get('/products/:id', (request, response) => {
+    const {
+        id
+    } = request.params;
+    const product = products.find(x => x.id === id)
+    response.send(product)
+})
+
+//     cadastro
+server.post('/products', (request, response) => {
+    const {
+        title,
+        author,
+        publisher,
+        price,
+        year
+    } = request.body;
+
+    const id = v4();
+    const product = {
+        id,
+        title,
+        author,
+        publisher,
+        price,
+        year
+    }
+
+    products.push(product)
+    response.send({
+        content: title
+    })
+})
+
+//       edição
+server.put('/products/:id', (request, response) => {
+    const {
+        id
+    } = request.params;
+    const {
+        title,
+        author,
+        publisher,
+        price,
+        year
+    } = request.body
+
+    const productIndex = products.findIndex(x => x.id === id)
+    const product = {
+        id,
+        title,
+        author,
+        publisher,
+        price,
+        year
+    }
+    products[productIndex] = product;
+
+    response.send(product)
+});
+
+//    delete
+server.delete('/products/:id', (request, response) => {
+    const {
+        id
+    } = request.params;
+    products = products.filter(x => x.id !== id)
+    response.send({})
+})
+
+
+
 
 const PORT = 3333;
 server.listen(PORT, () => console.log(`~*~ Server listening on PORT: ${PORT}. ~*~`));
