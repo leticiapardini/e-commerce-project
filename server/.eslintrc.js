dbdd00181cd2b2bf12a22d666a980e0cea635771
murillo.costa@live.com <murillo.costa@live.com>
2022-06-10 22:57:02 -0300

    fix

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -11,8 +11,8 @@ module.exports = {
     },
     rules: {
       'no-var': 'error',
-      semi: 'error',
-      indent: ['error', 2, { SwitchCase: 1 }],
+      "semi": 'error',
+      "indent": ['error', 2, { SwitchCase: 1 }],
       'no-multi-spaces': 'error',
       'space-in-parens': 'error',
       'no-multiple-empty-lines': 'error',
